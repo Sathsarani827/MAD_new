7b44e5f7f3f0c64931d3a0b46f542a11ec703ee6
sathsarani827 <it19023038@my.sliit.lk>
2020-08-16 23:32:13 +0530

    design page

---
+++ app/src/main/java/com/example/bustracker/Aggreement.java
@@ -0,0 +1,14 @@
+package com.example.bustracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Aggreement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aggreement);
+    }
+}
